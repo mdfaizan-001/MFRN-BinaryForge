9ffb17582c71818044f34be0442f6ff3deb44f91
Md Faizan <mfrnmdfaizan@gmail.com>
2026-06-11 19:15:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function convertToBinary() {
+
+    let input =
+    document.getElementById("decimalInput").value;
+
+    let result =
+    document.getElementById("binaryResult");
+
+    if (input === "") {
+
+        result.innerHTML =
+        "Please enter a valid decimal number";
+
+        return;
+    }
+
+    let decimal = parseInt(input);
+
+    let binary = decimal.toString(2);
+
+    result.innerHTML = binary;
+}
+
+document
+.getElementById("decimalInput")
+.addEventListener("keypress", function(e) {
+
+    if (e.key === "Enter") {
+        convertToBinary();
+    }
+
+});
